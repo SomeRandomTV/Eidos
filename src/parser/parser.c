b8475aa677df0b4d4c16ced214981814f063343d
Turd Ferguson <alejoserubio@gmail.com>
2025-12-25 19:00:14 -0700

    Bulding Syntax Parser

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -4,22 +4,90 @@
 
 /* ========== PRIVTATE functions ========== */
 
-static ASTNode* parse_stmts(Parser* parser);
-static ASTNode* parse_stmt(Parser* parser);
-static ASTNode* parse_var_decl(Parser* parser);
-static ASTNode* parse_assignment_stmt(Parser* parser);
-static ASTNode* parse_if_stmt(Parser* parser);
-static ASTNode* parse_loop_stmt(Parser* parser);
-static ASTNode* parse_io_stmt(Parser* parser);
-static ASTNode* parse_inc_dec_stmt(Parser* parser);
-static ASTNode* parse_expr(Parser* parser);
-static ASTNode* parse_term(Parser* parser);
-static ASTNode* parse_factor(Parser* parser);
-static ASTNode* parse_conditional(Parser* parser);
-static void advance(Parser* parser);
-static bool match(Parser* parser, TokenType type);
-static bool expect(Parser* parser, TokenType type);
-static void parser_error(Parser* parser, const char* message);
+static ASTNode* parse_stmts(Parser* parser) {
+
+
+}
+
+static ASTNode* parse_stmt(Parser* parser) {
+
+}
+
+static ASTNode* parse_var_decl(Parser* parser) { 
+
+}
+
+static ASTNode* parse_assignment_stmt(Parser* parser) {
+
+}
+
+static ASTNode* parse_if_stmt(Parser* parser) {
+
+}
+
+static ASTNode* parse_loop_stmt(Parser* parser) {
+
+}
+
+static ASTNode* parse_io_stmt(Parser* parser) {
+
+}
+
+static ASTNode* parse_inc_dec_stmt(Parser* parser) {
+
+}
+
+static ASTNode* parse_expr(Parser* parser) {
+
+}
+
+static ASTNode* parse_term(Parser* parser) {
+
+}
+
+static ASTNode* parse_factor(Parser* parser) {
+
+}
+
+static ASTNode* parse_conditional(Parser* parser) {
+
+}
+
+static void advance(Parser* parser) {
+    /*
+    Advances the parser throughout the tokens 
+    */
+
+    // Free the old current_token's lexeme (parser owns the memory)
+    if (parser->current_token.lexeme) {
+        free((void*)parser->current_token.lexeme);
+    }
+    
+    // Move peek to current
+    parser->current_token = parser->peek_token;
+    
+    // Get new peek token from lexer
+    parser->peek_token = next_token(parser->lexer);
+}
+
+
+static bool match(Parser* parser, TokenType expectedType) {
+    
+    if (parser->current_token.tokenType != expectedType) {
+        parser_error(parser);
+        return false;
+
+    } else {
+        return true;
+    }
+
+}
+
+static void parser_error(Parser* parser) {
+
+    if 
+
+}
 
 
 /* ========== PUBLIC API ========== */
